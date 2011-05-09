4e8aa0b3695e5c070515f3a6e52f3586efc04b98
Derek <drod2169@gmail.com>
2011-05-09 04:22:56 -0400

    KangBang first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 7 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 PREEMPT Mon May 9 03:39:16 EDT 2011"
+#define LINUX_COMPILE_TIME "03:39:16"
+#define LINUX_COMPILE_BY "drod2169"
+#define LINUX_COMPILE_HOST "drod2169"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
